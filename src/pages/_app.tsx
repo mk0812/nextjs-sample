95f924dcafa0cfa551ed021bb0c96596a2933226
mk0812 <matsuoka812930@gmail.com>
2021-07-18 21:35:38 +0900

    💄

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
-import 'tailwindcss/tailwind.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
+import { NextPage } from 'next'
+import { AppProps } from 'next/dist/next-server/lib/router/router'
+
+const MyApp: NextPage<AppProps> = ({Component, pageProps}: AppProps) => {
   return <Component {...pageProps} />
 }
+
 export default MyApp
