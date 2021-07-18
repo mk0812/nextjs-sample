a1eed54ed314c6d416496cacc0fd9f5732e2b5b3
mk0812 <matsuoka812930@gmail.com>
2021-07-18 21:28:28 +0900

    add: comment

--- jest.config.js
+++ jest.config.js
@@ -21,6 +21,7 @@ module.exports = {
     // we must specify a custom tsconfig for tests because we need the typescript transform
     // to transform jsx into js rather than leaving it jsx such as the next build requires. you
     // can see this setting in tsconfig.jest.json -> "jsx": "react"
+    // @see https://github.com/vercel/next.js/issues/8663
     'ts-jest': {
       tsconfig: '<rootDir>/test/tsconfig.jest.json',
     },
