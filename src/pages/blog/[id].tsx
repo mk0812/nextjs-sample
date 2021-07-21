f168ac4ed87f944f4887d2e93b76146784592bf7
mk0812 <matsuoka812930@gmail.com>
2021-07-22 00:25:27 +0900

    eslint

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -1,4 +1,4 @@
-import { client } from "../../libs/client";
+import { client } from '../../libs/client'
 import { GetStaticPropsContext } from 'next'
 
 export default function BlogId({ blog }: any) {
@@ -12,25 +12,27 @@ export default function BlogId({ blog }: any) {
         }}
       />
     </main>
-  );
+  )
 }
 
 // 静的生成のためのパスを指定します
 export const getStaticPaths = async () => {
-  const data = await client.get<{ contents: {id: string}[] }>({ endpoint: "blog" });
+  const data = await client.get<{ contents: { id: string }[] }>({
+    endpoint: 'blog',
+  })
 
-  const paths = data.contents.map((content) => `/blog/${content.id}`);
-  return { paths, fallback: false };
-};
+  const paths = data.contents.map((content) => `/blog/${content.id}`)
+  return { paths, fallback: false }
+}
 
 // データをテンプレートに受け渡す部分の処理を記述します
 export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params?.id as string
-  const data = await client.get({ endpoint: "blog", contentId: id });
+  const data = await client.get({ endpoint: 'blog', contentId: id })
 
   return {
     props: {
       blog: data,
     },
-  };
-};
+  }
+}
