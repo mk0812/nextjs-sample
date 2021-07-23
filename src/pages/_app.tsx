bd7e1742587adde1ce435e1e45d699aa18603dec
mk0812 <matsuoka812930@gmail.com>
2021-07-23 11:49:44 +0900

    gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,26 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
 
 import { NextPage } from 'next'
 import { AppProps } from 'next/dist/next-server/lib/router/router'
+import { useRouter } from 'next/router'
+
 import { CommonMeta } from '../components/Head'
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      console.log('handleRouteChange')
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       <CommonMeta
